30cd8d09d312e8771bd3ed94c2260b5d44835e6f
Jubayth Hossen Roni <84420078+rony9843@users.noreply.github.com>
2023-01-02 16:55:32 +0300

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "*",
+    // origin: "*",
+    origin: [
+      "https://rony9843.github.io/Queenz-Zone-dashboard/",
+      "https://rony9843.github.io/",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
